d302c55efe6c7f3dc639fa0ee234c0a37fbad4a6
plamenyovchev <plamenkooo@gmail.com>
2020-07-25 16:02:13 +0300

    CarsContainer initial commit

---
+++ src/components/CarsContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CarsContainer = () => {
+  return (
+    <>
+      <h2>Available Cars</h2>
+      <button>Buy Car</button>
+    </>
+  );
+};
+
+export default CarsContainer;
