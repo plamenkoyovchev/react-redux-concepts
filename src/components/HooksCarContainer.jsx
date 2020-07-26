536353e54cf030e9e1f772a98903c230f5f13dee
plamenyovchev <plamenkooo@gmail.com>
2020-07-26 09:49:43 +0300

    Dispatch action creator with useDispatch

--- src/components/HooksCarContainer.jsx
+++ src/components/HooksCarContainer.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+
+import { buyCar } from "../store";
 
 const HooksCarContainer = () => {
   const availableCars = useSelector((state) => state.availableCars);
+  const dispatch = useDispatch();
   return (
     <>
       <h2>Hooks Available Cars - {availableCars}</h2>
-      <button>Buy Car</button>
+      <button onClick={() => dispatch(buyCar)}>Buy Car</button>
     </>
   );
 };
