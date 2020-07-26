1aac3fee78e0166b0a1cf197b6ce1249323d2105
plamenyovchev <plamenkooo@gmail.com>
2020-07-26 09:53:25 +0300

    Add HooksCarContainer to App

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Provider } from "react-redux";
 import store from "./store/store";
 
 import CarsContainer from "./components/CarsContainer";
+import HooksCarContainer from './components/HooksCarContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <CarsContainer />
+        <HooksCarContainer />
       </div>
     </Provider>
   );
