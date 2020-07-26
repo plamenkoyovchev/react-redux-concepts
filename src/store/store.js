a1baed1e18602aa5d8258b3cab45de358c927672
plamenyovchev <plamenkooo@gmail.com>
2020-07-26 10:22:07 +0300

    use rootReducer from store

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux';
-import carReducer from "./cars/carReducer";
+import rootReducer from "./rootReducer";
 
-const store = createStore(carReducer);
+const store = createStore(rootReducer);
 
 export default store;
