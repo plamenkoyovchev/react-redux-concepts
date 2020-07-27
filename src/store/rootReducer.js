7800d08f06a522e37f288bc95083b79302493b74
plamenyovchev <plamenkooo@gmail.com>
2020-07-27 13:42:41 +0300

    Register userReducer in rootReducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import carReducer from "./cars/carReducer";
 import motorReducer from "./motors/motorReducer";
+import usersReducer from "./users/userReducer";
 
 const rootReducer = combineReducers({
     cars: carReducer,
-    motors: motorReducer
+    motors: motorReducer,
+    users: usersReducer
 });
 
 export default rootReducer;
